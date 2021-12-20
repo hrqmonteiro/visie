be482edce596a885c8bdb8523d794564b5c3375f
Hrq <hrqmonteiro@protonmail.com>
2021-12-20 18:48:05 -0300

    added vue front

--- server/routes/feed.js
+++ server/routes/feed.js
@@ -4,19 +4,19 @@ const feedController = require('../controllers/feed')
 
 const router = express.Router()
 
-// GET /feed/persons GET PERSONS
+// GET /feed/persons GET PERSONS - fetch todas pessoas
 router.get('/persons', feedController.getPersons)
 
-// GET / feed/person/id GER PERSON
+// GET / feed/person/id GER PERSON - pessoa individual
 router.get('/person/:personId', feedController.getPerson)
 
-// POST /feed/person CREATE PERSON
+// POST /feed/person CREATE PERSON - criar pessoa
 router.post('/person', feedController.postPerson)
 
-// PUT /feed/person/id UPDATE PERSON
+// PUT /feed/person/id UPDATE PERSON - atualizar a pessoa
 router.put('/person/:personId', feedController.updatePerson)
 
-// DELETE /feed/person/id
+// DELETE /feed/person/id - deletar a pessoa
 router.delete('/person/:personId', feedController.deletePerson)
 
 module.exports = router
